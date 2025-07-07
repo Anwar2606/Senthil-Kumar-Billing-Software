794855dc2b77515fddbe751a571150513f27acd1
Anwar2606 <hfanwar26@gmail.com>
2025-07-07 16:26:10 +0530

    first commit

---
+++ src/pages/Sidebar/Sidebar.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Sidebar.css";
+import {
+  FaHome, FaEye, FaEdit, FaFileInvoice, FaArrowAltCircleRight,
+  FaArrowCircleLeft, FaChevronDown, FaChevronUp
+} from "react-icons/fa";
+import { AiFillProduct } from "react-icons/ai";
+import { TbListNumbers } from "react-icons/tb";
+import { HiOutlineDocumentCheck } from "react-icons/hi2";
+import { IoIosPerson } from "react-icons/io";
+import { HiOutlineDocumentText } from "react-icons/hi2";
+import { MdLogout } from "react-icons/md";
+import { GrDocumentPdf } from "react-icons/gr";
+import { IoDocumentTextOutline } from "react-icons/io5";
+import Logo from "../assets/PCW.png"; // Update path if needed
+import MyLogo from '../assets/SF logo.png';
+
+const Sidebar = ({ isOpen,isOpen2, toggleSidebar }) => {
+  const [invoiceOpen, setInvoiceOpen] = useState(false);
+   const [isBillsOpen, setIsBillsOpen] = useState(false);
+   const [isBillsOpen2, setIsBillsOpen2] = useState(false);
+  const toggleBillsSubMenu = () => {
+    setIsBillsOpen(!isBillsOpen);
+  };
+const toggleBillsSubMenu2 = () => {
+    setIsBillsOpen2(!isBillsOpen);
+  };
+  return (
+    <div className={`sidebar ${isOpen ? "open" : "collapsed"}`}>
+      <ul>
+        <li style={{ fontSize: '22px', fontWeight: 'bold', textAlign: 'center', margin: '20px 0', borderBottom: '2px solid #c3b6d0', paddingBottom: '10px' }}>
+          {isOpen ? 'Senthil Kumar' : (
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginLeft: '-14px' }}>
+              <img src={MyLogo} alt="PCW Logo" style={{ width: '50px', height: 'auto' }} />
+            </div>
+          )}
+        </li>
+        <li><Link to="/newhome"><FaHome /> {isOpen && <span>Home</span>}</Link></li>
+        <li><Link to="/products"><AiFillProduct /> {isOpen && <span>Products</span>}</Link></li>
+        {/* <li><Link to="/allbills"><FaEye /> {isOpen && <span>All Bills</span>}</Link></li> */}
+         <li onClick={toggleBillsSubMenu} style={{ cursor: 'pointer' }}>
+        <FaEye /> {isOpen && <span>All Bills ▾</span>}
+      </li>
+      {isBillsOpen && isOpen && (
+        <ul style={{ listStyle: 'none', paddingLeft: '20px' }}>
+          <li><Link to="/wholesalecopy"><GrDocumentPdf /> Whole Sale Copy</Link></li>
+          <li><Link to="/retailcopy"><IoDocumentTextOutline /> Retail Copy</Link></li>
+          <li><Link to="/invoicecopy"><HiOutlineDocumentText /> Invoice Copy</Link></li>
+          <li><Link to="/waybillcopy"><HiOutlineDocumentCheck /> Way Bill Copy</Link></li>
+        </ul>
+      )}
+      <li onClick={toggleBillsSubMenu2} style={{ cursor: 'pointer' }}>
+        <FaEdit /> {isOpen && <span>Edit Bills ▾</span>}
+      </li>
+       {isBillsOpen2 && isOpen && (
+        <ul style={{ listStyle: 'none', paddingLeft: '20px' }}>
+          <li><Link to="/wholesaleeditbill"><GrDocumentPdf /> Wholesale Copy</Link></li>
+          <li><Link to="/retaileditbill"><IoDocumentTextOutline /> Retail Copy</Link></li>
+          <li><Link to="/invoiceeditbill"><HiOutlineDocumentText /> Invoice Copy</Link></li>
+          <li><Link to="/waybilleditbill"><HiOutlineDocumentCheck /> Way Bill Copy</Link></li>
+        </ul>
+      )}
+         <li><Link to="/wholesalebill"><GrDocumentPdf />{isOpen && <span>Whole Sale Bill</span>}</Link></li>
+        <li><Link to="/retailcalculator"><IoDocumentTextOutline />{isOpen &&<span>Retail Bill</span>}</Link></li>
+         <li><Link to="/invoicebill"><HiOutlineDocumentText />{isOpen &&<span>Invoice</span>}</Link></li>
+        <li><Link to="/waybill"><HiOutlineDocumentCheck />{isOpen &&<span>Way Bill</span>}</Link></li>
+        <li><Link to="/showcustomers"><IoIosPerson /> {isOpen && <span>Customers</span>}</Link></li>
+        <li><Link to="/invoice"><TbListNumbers />{isOpen && <span>Invoice Numbers</span>}</Link></li>
+        <li><Link to="/"><MdLogout /> {isOpen && <span>Logout</span>}</Link></li>
+        <li className="menu-item">
+          <button onClick={toggleSidebar} style={{ padding: '10px', backgroundColor: '#1b2594', border: 'none', borderRadius: '50%', display: 'flex', alignItems: 'center', justifyContent: 'center', width: '40px', height: '40px', cursor: 'pointer' }}>
+            {isOpen ? <FaArrowCircleLeft style={{ color: 'white' }} /> : <FaArrowAltCircleRight style={{ color: 'white' }} />}
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/pages/firebase.js
@@ -0,0 +1,44 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { getAuth } from 'firebase/auth'; 
+
+const firebaseConfig = {
+  //testing
+  apiKey: "AIzaSyCTmFMUSQL_lvxZSGzihrx5G7AypB4Uk5Q",
+  authDomain: "testing-855ce.firebaseapp.com",
+  projectId: "testing-855ce",
+  storageBucket: "testing-855ce.appspot.com",
+  messagingSenderId: "1086229411180",
+  appId: "1:1086229411180:web:4a835dadcfb73b08a42f49" 
+  
+    //main
+    // apiKey: "AIzaSyD9dhzJrhhENssGVbsGleNDk7ZTBUxsyik",
+    // authDomain: "mainbilling1-166c9.firebaseapp.com",
+    // projectId: "mainbilling1-166c9",
+    // storageBucket: "mainbilling1-166c9.appspot.com",
+    // messagingSenderId: "411663637638",
+    // appId: "1:411663637638:web:d7b7c7221860edcba74507"
+  //main2
+  // apiKey: "AIzaSyC7CD7ve78jS2k3a3XzkdlWwBGhp_9Lfg0",
+  // authDomain: "billing-software2.firebaseapp.com",
+  // projectId: "billing-software2",
+  // storageBucket: "billing-software2.appspot.com",
+  // messagingSenderId: "97163152398",
+  // appId: "1:97163152398:web:b02de4e1e085efbca9c05f"
+
+  //main db
+// apiKey: "AIzaSyBvSxMdmwBjPssexWqhULFPj8BVlv55mqc",
+//   authDomain: "senthil-kumar-fireworks.firebaseapp.com",
+//   projectId: "senthil-kumar-fireworks",
+//   storageBucket: "senthil-kumar-fireworks.firebasestorage.app",
+//   messagingSenderId: "592291931620",
+//   appId: "1:592291931620:web:f39203c5003263d53f93f1"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app); 
+const storage = getStorage(app); 
+const auth = getAuth(app); 
+const firestore = getFirestore(app);
+export { db, storage, auth , firestore}; 

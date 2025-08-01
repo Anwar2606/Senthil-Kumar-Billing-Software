3d8aad8f3ff1f2de7a4ccf43088667e1cad86a16
Anwar2606 <hfanwar26@gmail.com>
2025-08-01 12:17:19 +0530

    firebase change

--- src/pages/firebase.js
+++ src/pages/firebase.js
@@ -5,12 +5,12 @@ import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   //testing
-  apiKey: "AIzaSyCTmFMUSQL_lvxZSGzihrx5G7AypB4Uk5Q",
-  authDomain: "testing-855ce.firebaseapp.com",
-  projectId: "testing-855ce",
-  storageBucket: "testing-855ce.appspot.com",
-  messagingSenderId: "1086229411180",
-  appId: "1:1086229411180:web:4a835dadcfb73b08a42f49" 
+  // apiKey: "AIzaSyCTmFMUSQL_lvxZSGzihrx5G7AypB4Uk5Q",
+  // authDomain: "testing-855ce.firebaseapp.com",
+  // projectId: "testing-855ce",
+  // storageBucket: "testing-855ce.appspot.com",
+  // messagingSenderId: "1086229411180",
+  // appId: "1:1086229411180:web:4a835dadcfb73b08a42f49" 
   
     //main
     // apiKey: "AIzaSyD9dhzJrhhENssGVbsGleNDk7ZTBUxsyik",
@@ -28,12 +28,12 @@ const firebaseConfig = {
   // appId: "1:97163152398:web:b02de4e1e085efbca9c05f"
 
   //main db
-// apiKey: "AIzaSyBvSxMdmwBjPssexWqhULFPj8BVlv55mqc",
-//   authDomain: "senthil-kumar-fireworks.firebaseapp.com",
-//   projectId: "senthil-kumar-fireworks",
-//   storageBucket: "senthil-kumar-fireworks.firebasestorage.app",
-//   messagingSenderId: "592291931620",
-//   appId: "1:592291931620:web:f39203c5003263d53f93f1"
+apiKey: "AIzaSyBvSxMdmwBjPssexWqhULFPj8BVlv55mqc",
+  authDomain: "senthil-kumar-fireworks.firebaseapp.com",
+  projectId: "senthil-kumar-fireworks",
+  storageBucket: "senthil-kumar-fireworks.firebasestorage.app",
+  messagingSenderId: "592291931620",
+  appId: "1:592291931620:web:f39203c5003263d53f93f1"
 };
 
 const app = initializeApp(firebaseConfig);
